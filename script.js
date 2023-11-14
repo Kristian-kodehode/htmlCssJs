ec2c774f42e836564511120a99ccb3ab8f16e5ab
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-11-14 18:08:35 +0100

    lots of updates testing sstuff out

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 const sidesLeft = document.querySelector(".sides-left");
 const sidesRight = document.querySelector(".sides-right");
+const descriptiontext = document.querySelector(".descriptiontext");
 
 const eventHandlerLeft = () => {
   console.log("Left Side clicked");
-  (sidesLeft.style.width = "95%") && (sidesRight.style.width = "5%");
+  if (window.innerWidth > 1080) {
+    sidesLeft.style.width = "95%";
+    sidesRight.style.width = "5%";
+  } else {
+    sidesLeft.style.height = "95%";
+
+    sidesRight.style.height = "5%";
+  }
 };
 
 const eventHandlerRight = () => {
   console.log("Right Side clicked");
-  (sidesLeft.style.width = "5%") && (sidesRight.style.width = "95%");
+  if (window.innerWidth > 1080) {
+    sidesLeft.style.width = "5%";
+    sidesRight.style.width = "95%";
+  } else {
+    sidesLeft.style.height = "5%";
+
+    sidesRight.style.height = "95%";
+  }
 };
 
 sidesLeft.addEventListener("click", eventHandlerLeft);
