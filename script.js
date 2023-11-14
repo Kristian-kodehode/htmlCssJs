69f705a36bf10b6f0c7cf8ecf5801ab1831fb6da
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-11-14 19:20:16 +0100

    doing some responsive stuff

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const sidesLeft = document.querySelector(".sides-left");
 const sidesRight = document.querySelector(".sides-right");
 const descriptiontext = document.querySelector(".descriptiontext");
+const resetbutton = document.querySelector(".resetbutton");
 
 const eventHandlerLeft = () => {
   console.log("Left Side clicked");
@@ -9,7 +10,6 @@ const eventHandlerLeft = () => {
     sidesRight.style.width = "5%";
   } else {
     sidesLeft.style.height = "95%";
-
     sidesRight.style.height = "5%";
   }
 };
@@ -21,10 +21,26 @@ const eventHandlerRight = () => {
     sidesRight.style.width = "95%";
   } else {
     sidesLeft.style.height = "5%";
-
     sidesRight.style.height = "95%";
   }
 };
 
+const resetEvents = () => {
+  if (window.innerWidth > 1080) {
+    sidesLeft.style.width = "50%";
+    sidesLeft.style.height = "100v%";
+
+    sidesRight.style.width = "50%";
+    sidesRight.style.height = "100v%";
+  } else {
+    sidesLeft.style.width = "100%";
+    sidesLeft.style.height = "50%";
+    sidesRight.style.width = "100%";
+    sidesRight.style.height = "50%";
+  }
+  console.log("Reset button clicked");
+};
+
 sidesLeft.addEventListener("click", eventHandlerLeft);
 sidesRight.addEventListener("click", eventHandlerRight);
+resetbutton.addEventListener("click", resetEvents);
