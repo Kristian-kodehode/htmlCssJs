80488031a1d8221171ef12ed8d405a093eeedec7
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-11-14 16:32:35 +0100

    sides now scales dependent of eachother

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const sidesLeft = document.getElementsByClassName("sides-left")[0];
-const sidesRight = document.getElementsByClassName("sides-right")[0];
+const sidesLeft = document.querySelector(".sides-left");
+const sidesRight = document.querySelector(".sides-right");
 
 const eventHandlerLeft = () => {
   console.log("Left Side clicked");
+  (sidesLeft.style.width = "95%") && (sidesRight.style.width = "5%");
 };
+
 const eventHandlerRight = () => {
   console.log("Right Side clicked");
+  (sidesLeft.style.width = "5%") && (sidesRight.style.width = "95%");
 };
 
 sidesLeft.addEventListener("click", eventHandlerLeft);
