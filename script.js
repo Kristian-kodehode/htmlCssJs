b4a17b98aaf823acfa51bd55676e55ee0816f739
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-11-23 14:38:31 +0100

    updated now the responsiveness works

--- script.js
+++ script.js
@@ -3,6 +3,29 @@ const sidesRight = document.querySelector(".sides-right");
 const descriptiontext = document.querySelector(".descriptiontext");
 const resetbutton = document.querySelector(".resetbutton");
 
+// const handleWindowResize = () => {
+//   resetEvents();
+// };
+
+// window.addEventListener("resize", handleWindowResize);
+
+let isWindowAboveThreshold = window.innerWidth > 1080;
+
+const handleWindowResize = () => {
+  const isNowAboveThreshold = window.innerWidth > 1080;
+
+  // Check if the window has crossed the 1080px threshold
+  if (isNowAboveThreshold !== isWindowAboveThreshold) {
+    resetEvents();
+    isWindowAboveThreshold = isNowAboveThreshold;
+  }
+};
+
+// Add an event listener for window resize
+window.addEventListener("resize", handleWindowResize);
+
+// Your existing code...
+
 const eventHandlerLeft = () => {
   console.log("Left Side clicked");
   if (window.innerWidth > 1080) {
@@ -28,10 +51,10 @@ const eventHandlerRight = () => {
 const resetEvents = () => {
   if (window.innerWidth > 1080) {
     sidesLeft.style.width = "50%";
-    sidesLeft.style.height = "100v%";
+    sidesLeft.style.height = "100%";
 
     sidesRight.style.width = "50%";
-    sidesRight.style.height = "100v%";
+    sidesRight.style.height = "100%";
   } else {
     sidesLeft.style.width = "100%";
     sidesLeft.style.height = "50%";
