8b8bf3817bd0be19cc359450662d89a6402c1c35
Kristian Kodehode <kristian007.kodehode@gmail.com>
2023-11-29 12:03:59 +0100

    works with scroll and resizing etc

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const sidesLeft = document.querySelector(".sides-left");
 const sidesRight = document.querySelector(".sides-right");
+const imageContainerLeft = document.querySelector(".imagecontainer-left");
+const imageContainerRight = document.querySelector(".imagecontainer-right");
 const descriptiontext = document.querySelector(".descriptiontext");
 const resetbutton = document.querySelector(".resetbutton");
 
@@ -24,16 +26,22 @@ const handleWindowResize = () => {
 // Add an event listener for window resize
 window.addEventListener("resize", handleWindowResize);
 
-// Your existing code...
-
 const eventHandlerLeft = () => {
   console.log("Left Side clicked");
   if (window.innerWidth > 1080) {
     sidesLeft.style.width = "95%";
     sidesRight.style.width = "5%";
+    sidesLeft.style.overflowY = "scroll";
+    sidesRight.style.overflowY = "hidden";
+    imageContainerRight.style.display = "none";
+    imageContainerLeft.style.display = "block";
   } else {
     sidesLeft.style.height = "95%";
     sidesRight.style.height = "5%";
+    sidesLeft.style.overflowY = "scroll";
+    sidesRight.style.overflowY = "hidden";
+    imageContainerRight.style.display = "none";
+    imageContainerLeft.style.display = "block";
   }
 };
 
@@ -42,14 +50,28 @@ const eventHandlerRight = () => {
   if (window.innerWidth > 1080) {
     sidesLeft.style.width = "5%";
     sidesRight.style.width = "95%";
+    sidesRight.style.overflowY = "scroll";
+    sidesLeft.style.overflowY = "hidden";
+    imageContainerRight.style.display = "block";
+    imageContainerLeft.style.display = "none";
   } else {
     sidesLeft.style.height = "5%";
     sidesRight.style.height = "95%";
+    sidesRight.style.overflowY = "scroll";
+    sidesLeft.style.overflowY = "hidden";
+    imageContainerRight.style.display = "block";
+    imageContainerLeft.style.display = "none";
   }
 };
 
 const resetEvents = () => {
   const isLargeScreen = window.innerWidth > 1080;
+
+  sidesRight.style.overflowY = "hidden";
+  sidesLeft.style.overflowY = "hidden";
+
+  imageContainerLeft.style.display = "none";
+  imageContainerRight.style.display = "none";
 
   sidesLeft.style.width = isLargeScreen ? "50%" : "100%";
   sidesLeft.style.height = isLargeScreen ? "100%" : "50%";
